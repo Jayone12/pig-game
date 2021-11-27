2d838ad88bdb0811fd1e9eefda06ecd629e86d4c
TJUN <jjkeb47@gmail.com>
2021-11-27 19:11:50 +0900

    feat: Create object roll button event
    Create an object to reference the element and a roll button event

--- pig-game.js
+++ pig-game.js
@@ -1,1 +1,41 @@
-'use strict';
+"use strict";
+
+/**
+ * --------------------
+ *    Roll Dice Game
+ * --------------------
+ */
+const rollDice = {
+  'dice' : document.querySelector('.dice'),                   // 주사위
+  'diceNum' : document.querySelector('.dice .num'),           // 주사위 값을 보여줄 요소
+  'winnerScoreEl' : document.querySelector('.winner-score'),  // 승리할 최종 값을 받을 input
+  'turn' : 1,                                                 // 누구 턴인지 확인
+  'btns' : {
+    'roll' : document.querySelector('.btn-roll'),             // Roll Dice Button
+    'hold' : document.querySelector('.btn-hold'),             // Hold Button
+  },
+  'player' : {                                                // 플래이어 점수 담을 객체
+    1:{
+      'total' : document.querySelector('.player1 .total-score .num'),
+      'current' : document.querySelector('.player1 .current-score .num'),
+    },
+    2:{
+      'total' : document.querySelector('.player2 .total-score .num'),
+      'current' : document.querySelector('.player2 .current-score .num'),
+    },
+  }
+};
+
+// dice roll event
+rollDice.btns.roll.addEventListener('click', function(){
+  const rollResult = Math.floor(Math.random() * 6)
+  rollDice.winnerScoreEl.disabled = true
+  rollDice.diceNum.textContent = rollResult
+  if (rollResult == 0) {
+    rollDice.dice.classList.add('oops')
+    setTimeout(function(){
+      rollDice.dice.classList.remove('oops')
+    }, 1000)
+  }
+  rollDice.player[rollDice.turn].current.textContent = Number(rollDice.player[rollDice.turn].current.textContent) + rollResult;
+});
