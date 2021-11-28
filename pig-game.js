2767c4b9ae07bae4e91f6269390341bc853de44c
talentforest <talentforest0501@gmail.com>
2021-11-28 17:18:24 +0900

    changed it to a function

--- pig-game.js
+++ pig-game.js
@@ -1,11 +1,16 @@
 "use strict";
 
 let currentScore = 0;
-
 const dice = Math.trunc(Math.random() * 6) + 1;
 
-if (dice !== 1) {
-  currentScore += dice;
-} else {
-  currentScore = 0;
+function rollDice() {
+  if (dice !== 1) {
+    currentScore += dice;
+  } else {
+    currentScore = 0;
+  }
+  return dice;
 }
+
+rollDice(dice);
+console.log(currentScore);
